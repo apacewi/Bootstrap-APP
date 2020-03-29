02e4ed26131f1bef7bf44515298550f4428b0274
Arkadiusz Pacewicz <arkadiusz.pacewicz.programista@gmail.com>
2020-03-29 23:51:11 +0200

    first commit bootstrap app

---
+++ zmiany.js
@@ -0,0 +1,49 @@
+var number = Math.floor(Math.random()*4)+1;
+var description='<div class="mess">Efektywnie zarządzaj własnym budżetem!</div>';
+
+function changeposition()
+{
+	
+	if (number == 1)
+		 description='<div class="mess2">Efektywnie zarządzaj własnym budżetem!</div>';
+	if (number == 2)
+		 description='<div class="mess3">Zadbaj o stabilny przyrost gotówki!</div>';
+	if (number == 3)
+		description='<div class="mess">Zrób użytek z każdego grosza, zamień go w prawdziwy majątek!</div>';
+	if (number == 4)
+		description='<div class="mess4">Regularność przerodzi się w sukces!</div>';
+}
+function hideslide()
+{
+	$("#slider").fadeOut(300);
+}
+
+function changeslide()
+{
+	number++; 
+	if (number > 4) number=1;
+	
+	var plik = "<img src=\"img/img"+number+".png\" width = 70% height = 350px />";
+	
+	changeposition();
+	document.getElementById("slider").innerHTML = plik+description;
+	
+	$("#slider").fadeIn(300);
+	
+	setTimeout("changeslide()", 6000);
+	
+	setTimeout("hideslide()",5700);
+}
+
+var points;// = [40, 100, 1, 5, 25, 10];
+//document.getElementById("demo").innerHTML = points;
+
+function sorttable() 
+{
+	
+	
+	
+	
+  points.sort(function(a, b){return b-a});
+  document.getElementById("demo").innerHTML = points;
+}
