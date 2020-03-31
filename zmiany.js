adcfff33bd1ea7bbaeaa870da68ef199af5d2c9f
Arkadiusz Pacewicz <arkadiusz.pacewicz.programista@gmail.com>
2020-03-31 23:02:16 +0200

    Added Login/register panel

--- zmiany.js
+++ zmiany.js
@@ -38,12 +38,21 @@ function changeslide()
 var points;// = [40, 100, 1, 5, 25, 10];
 //document.getElementById("demo").innerHTML = points;
 
-function sorttable() 
+function changetoregister() 
 {
+	var register = '<form class="p-2 my-2 bg-light text-black rounded"><div class="form-group"><label for="przykladowyEmail" class="text-center"> Imię </label><div class="input-group mb-2"><div class="input-group-prepend"><span class="input-group-text button-size" id="basic-addon1"><i class="icon-user"></i></span></div><input type="text" class="form-control" placeholder="Imię " aria-label="Username" aria-describedby="basic-addon1"></div><label for="przykladowyEmail" class="text-center"> Nazwisko </label><div class="input-group mb-2"><div class="input-group-prepend"><span class="input-group-text button-size" id="basic-addon1"><i class="icon-user"></i></span></div><input type="text" class="form-control" placeholder="Nazwisko " aria-label="Username" aria-describedby="basic-addon1"></div><label for="przykladowyEmail" class="text-center"> Adres Email </label><div class="input-group mb-1"><div class="input-group-prepend"><span class="input-group-text button-size" id="basic-addon1">@</span></div><input type="text" class="form-control" placeholder="Adres Email " aria-label="Username" aria-describedby="basic-addon1"></div><small id="podpowiedzEmail" class="form-text text-muted">W powyższym polu wpisujesz swój adres email.</small></div><div class="form-group"><label for="przykladoweHaslo">Hasło</label><div class="input-group mb-1"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1"><i class="icon-lock"></i></span></div><input type="password" class="form-control" placeholder="Hasło " aria-label="password" aria-describedby="basic-addon1"></div></div><div class = "text-center mt-2"><button type="submit" class="btn btn-primary"> Zarejestruj się </button></div></form>';
 	
+	var newtitle = '<h2>Od skutecznego oszczędzania dzieli Cię już tylko 1 krok.</h2>';
 	
+	document.getElementById("loginButt").innerHTML = register;
+	document.getElementById("paneltitle").innerHTML = newtitle;
+}
+function changetologin()
+{
+	var login = '<form id="loginButt" class="p-4 my-3 bg-light text-black rounded"><div class="form-group"><label for="przykladowyEmail" class="text-center"> Adres Email </label><div class="input-group mb-1"><div class="input-group-prepend"><span class="input-group-text button-size" id="basic-addon1">@</span></div><input type="text" class="form-control" placeholder="Adres Email " aria-label="Username" aria-describedby="basic-addon1"></div></div><div class="form-group"><label for="przykladoweHaslo">Hasło</label><div class="input-group mb-1"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1"><i class="icon-lock"></i></span></div><input type="password" class="form-control" placeholder="Hasło " aria-label="password" aria-describedby="basic-addon1"></div></div><div class="form-check"><input class="form-check-input" type="checkbox" id="autoSizingCheck2"><label class="form-check-label" for="autoSizingCheck2">Zapamiętaj mnie</label></div><div class = "text-center mt-3"><button type="submit" class="btn btn-primary"> Zaloguj się </button></div></form>';
 	
+	var newtitle = '<h2>Zaloguj się na własne darmowe konto!</h2>';
 	
-  points.sort(function(a, b){return b-a});
-  document.getElementById("demo").innerHTML = points;
+	document.getElementById("loginButt").innerHTML = login;
+	document.getElementById("paneltitle").innerHTML = newtitle;
 }
